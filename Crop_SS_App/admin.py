b5fc22c5d424cecdf7a0aebea0d628f601a711a0
jishnasomanath <64722324+jishnasomanath@users.noreply.github.com>
2021-12-21 13:32:07 +0530

    Add files via upload

---
+++ Crop_SS_App/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import pesticide_order_table
+admin.site.register(pesticide_order_table)
+
+
+# Register your models here.
---
+++ Crop_SS_App/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CropSsAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Crop_SS_App'
